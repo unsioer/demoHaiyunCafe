2d5592f357935794c0010fd1a57fcaa5e032969b
enatsu <unsioer@outlook.com>
2020-06-23 10:13:31 +0800

    Initial commit

---
+++ src/test/java/com/example/demoHaiyunCafe/DemoHaiyunCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoHaiyunCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHaiyunCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
