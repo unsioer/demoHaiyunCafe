a72051672f8f1c308ad942eded49b1407af3aa73
Nonoor <50571420+Nonoor@users.noreply.github.com>
2020-07-07 11:40:54 +0800

    add cart

---
+++ src/main/java/com/example/demoHaiyunCafe/Service/CartService.java
@@ -0,0 +1,20 @@
+package com.example.demoHaiyunCafe.Service;
+
+import com.example.demoHaiyunCafe.Bean.Cart;
+import com.example.demoHaiyunCafe.Bean.Item;
+
+import java.util.List;
+
+public interface CartService {
+
+    List<Cart> findAll();
+
+    Cart findById(Integer id);
+
+    Cart saveOrUpdateCart(Cart cart);
+
+    void deleteById(Integer id);
+
+    List<Cart> findAllByUid(Integer uid);
+
+}
