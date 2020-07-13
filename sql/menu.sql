7687a038b24ddb5848849dd484fa02d74367a0f7
smy-xmu <61258173+smy-xmu@users.noreply.github.com>
2020-07-13 11:44:13 +0800

    菜单创建中增加了默认餐点

--- sql/menu.sql
+++ sql/menu.sql
@@ -11,3 +11,15 @@ CREATE TABLE `menu` (
   `picturepath` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `menu`(`id`, `itemname`, `number`, `price`, `type`, `picturepath`)
+  VALUES(1, '拿铁', 100, 18, '饮料', 'img/拿铁.jpg');
+INSERT INTO `menu`(`id`, `itemname`, `number`, `price`, `type`, `picturepath`)
+  VALUES(2, '抹茶蛋糕', 66, 21, '小吃', 'img/抹茶蛋糕.jpg');
+INSERT INTO `menu`(`id`, `itemname`, `number`, `price`, `type`, `picturepath`)
+  VALUES(3, '意式肉酱面', 200, 25, '主食', 'img/意式肉酱面.jpg');
+INSERT INTO `menu`(`id`, `itemname`, `number`, `price`, `type`, `picturepath`)
+  VALUES(4, '慕斯蛋糕', 88, 19, '小吃', 'img/慕斯蛋糕.jpg');
+INSERT INTO `menu`(`id`, `itemname`, `number`, `price`, `type`, `picturepath`)
+  VALUES(5, '柠檬水', 250, 10, '饮料', 'img/柠檬水.jpg');
+  
