a23d9e6d3ad89581072c17a0b421c53c6bbd4309
unsioer <unsioer@outlook.com>
2020-07-21 20:04:21 +0800

    delete toastr

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -8,21 +8,3 @@ if (url.indexOf("newsCategoryManage") > 0
 	$("#news").addClass("active");
 	$("#newsManage").addClass("active");
 }
-
-// 提示条配置
-/*toastr.options = {
-	"closeButton" : true,
-	"debug" : false,
-	"progressBar" : true,
-	"preventDuplicates" : false,
-	"positionClass" : "toast-top-right",
-	"onclick" : null,
-	"showDuration" : "400",
-	"hideDuration" : "1000",
-	"timeOut" : "7000",
-	"extendedTimeOut" : "1000",
-	"showEasing" : "swing",
-	"hideEasing" : "linear",
-	"showMethod" : "fadeIn",
-	"hideMethod" : "fadeOut"
-}*/
